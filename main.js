a889f5cd876129f0c67fe347d504f2cabe7301ee
brunofelipehp <brunofelipehp8@gmail.com>
2022-04-18 19:32:59 -0300

    Utilizando o metodo DELETE

--- main.js
+++ main.js
@@ -44,6 +44,18 @@ function updateUser(updatedUser, id) {
     .catch(error => console.error(error))
 }
 
+function deleteUser(id) {
+    fetch(`${url}/${id}`, {
+        method: 'DELETE',
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+    .then(response => response.json())
+    .then(data => alertApi.textContent = data)
+    .catch(error => console.error(error))
+}
+
 
 const newUser = {
     name: "Bruno Felipe",
@@ -57,7 +69,9 @@ const newUser = {
      city: "Recife"
  }
 
-updateUser(updatedUser, 1)
-addUser(newUser)
+
+//updateUser(updatedUser, 1)
+//addUser(newUser)
+deleteUser(3)
 getUsers()
 getUser(2)
