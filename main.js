7131c2e113706447fd8a0b693f634acc30fb4711
brunofelipehp <brunofelipehp8@gmail.com>
2022-04-18 18:49:07 -0300

    Utilizando o metodo PUT

--- main.js
+++ main.js
@@ -7,15 +7,57 @@ function getUsers() {
     .catch(error => console.error(error))
 }
 
-function getUser(){
-    fetch(`${url}/1`)
+function getUser(id){
+    fetch(`${url}/${id}`)
     .then(response => response.json())
     .then(data => {
         userName.textContent = data.name
         userCity.textContent = data.city
         userAvatar.src = data.avatar
     })
+    .catch(error => console.error(error))
 }
 
+function addUser(newUser){
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(newUser),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+    .then(response => response.json())
+    .then(data => alertApi.textContent = data)
+    .catch(error => console.error(error))
+}
+function updateUser(updatedUser, id) {
+    fetch(`${url}/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify(updatedUser),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+        }
+
+    })
+    .then(response => response.json())
+    .then(data => alertApi.textContent = data)
+    .catch(error => console.error(error))
+}
+
+
+const newUser = {
+    name: "Bruno Felipe",
+    avatar: "https://avatars.githubusercontent.com/u/28022475?v=4",
+    city: "São Paulo"
+}
+
+ const updatedUser = {
+     name: "Marcelo Clovis",
+     avatar: "https://picsum.photos/200/300",
+     city: "Recife"
+ }
+
+updateUser(updatedUser, 1)
+addUser(newUser)
 getUsers()
-getUser()
+getUser(2)
